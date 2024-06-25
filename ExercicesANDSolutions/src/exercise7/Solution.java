97eace23adecb1e67a9d4cfffb2faba50c48b497
asmaeaouassar20 <asmaeaouassar333@gmail.com>
2024-06-25 17:22:19 +0100

    exercise 7

---
+++ ExercicesANDSolutions/src/exercise7/Solution.java
@@ -0,0 +1,68 @@
+package exercise7;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Solution {
+
+    /**
+     * This method stores the data that exists in the file in two arrays provided as parameters
+     * @param Products array of product [string]
+     * @param Prices array of prices of products [float]
+     */
+    public void storeInArray(String[] Products,float[] Prices){
+
+        if(Products.length!=Prices.length){
+            System.out.println("lengths of the two arrays must be equal");
+            return;
+        }
+
+        int n=Products.length;
+        String filePath="C://Users//PC//Desktop//learnJavaWithAsmae/products.txt";
+        FileReader fileReader= null;
+        try {
+            fileReader = new FileReader(filePath);
+            BufferedReader bufferedReader=new BufferedReader(fileReader);
+            String line=bufferedReader.readLine();
+
+            int productCounter=0,priceCounter=0; // used to add products and prices in arrays
+            boolean[] productAndPrice={false,false}; // to verify that each line in the file contains a product and its price
+            int i=0;
+
+            while(line!=null && i<n){
+                productAndPrice[0]=false;
+                productAndPrice[1]=false;
+                String[] wordsOfLine=line.split("-");
+                for(String w:wordsOfLine){
+                    if(stringIsFloat(w)){
+                        float price=Float.parseFloat(w);
+                        Prices[priceCounter++]=price;
+                        productAndPrice[1]=true;
+                    }else{
+                        Products[productCounter++]=w;
+                        productAndPrice[0]=true;
+                    }
+                }
+                if(productAndPrice[0]==false || productAndPrice[1]==false){
+                    System.out.println("missed product or price");
+                    return;
+                }
+                    line=bufferedReader.readLine();
+                i++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public boolean stringIsFloat(String s){
+        try{
+            Float.parseFloat(s);
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
